1347542805195ee22491fd9aab102d3eb69abb6f
Marana Caon <maranavaal@gmail.com>
2020-06-03 16:28:45 -0300

    exercícios de interpretação

---
+++ semana3/condicionais/index.js
@@ -0,0 +1,62 @@
+// EXERCÍCIO 2
+const respostaDoUsuario = prompt("Digite o número que você quer testar?")
+const numero = Number(respostaDoUsuario)
+
+if(numero % 2 === 0) {
+  console.log("Passou no teste.")
+} else {
+  console.log("Não passou no teste.")
+}
+
+// Resposta: Ele realiza o teste de quais números são divididos por 2. Números pares passam no teste
+// e número ímpares não.
+
+
+//EXERCÍCIO 2
+let fruta = prompt("Escolha uma fruta")
+let preco
+switch (fruta) {
+  case "Laranja":
+    preco = 3.5
+    break;
+  case "Maçã":
+    preco = 2.25
+    break;
+  case "Uva":
+    preco = 0.30
+    break;
+  case "Pêra":
+    preco = 5.5
+    break; // BREAK PARA O ITEM d.
+  default:
+    preco = 5
+    break;
+}
+console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
+
+// Resposta: a) Para atribuir um preço de acordo com o produto, no caso frutas diversas.
+// b) O preço da fruta maçã é R$ 2.25
+// c) 24,55. O preço da bananas é contado como default.
+// d) Ele conta como default e o preço da Pêra sai R$ 5.
+
+
+// EXERCÍCIO 3
+const numero1 = prompt("Digite o primeiro número.")
+const numero2 = prompt("Digite o próximo número?")
+
+if(numero1 > 0 && numero2 > 0) {
+  let mensagem
+  if(numero1 > numero2) {
+    mensagem = "Número 1 é maior que o 2!"
+  } else {
+    mensagem = "Número 1 é menor ou igual ao 2!"
+  }
+}
+
+console.log(mensagem)
+
+// Resposta
+
+
+
+
