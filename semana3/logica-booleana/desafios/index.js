2a7a820d66f816027a9e587fb8e50e38eec3d5ff
Marana Caon <maranavaal@gmail.com>
2020-06-02 22:43:42 -0300

    desafios

---
+++ semana3/logica-booleana/desafios/index.js
@@ -0,0 +1,39 @@
+// desafios
+// a)
+let libras = 20 
+let quilogramas1 = 2.2046
+let conversaoLibraParaQuilos = libras / quilogramas1
+console.log("20 lb equivalem à: " + conversaoLibraParaQuilos) //9.071940488070398
+
+// b)
+let oncas =  15.05
+let quilogramas2 = 0.0283495
+let conversaoOncasParaQuilos = oncas * quilogramas2
+console.log("15.05 oz equivalem à: " + conversaoOncasParaQuilos) // 0.426659975
+
+// c)
+let milhas = 100
+let metros = milhas * (201168 / 125) 
+console.log("100 mi equivalem à: " + metros) // 160934.4
+
+// d)
+let pes = 50
+let metros2 = 0.3048
+let conversaoPesParaMetros = pes * metros2
+console.log("50ft equivalem à: " + conversaoPesParaMetros) // 15.24
+
+// e)
+let galoes = 103.56
+let litros = 3.785
+let conversaoGaloesParaLitros = galoes * litros
+console.log("103.56gal equivalem à: " + conversaoGaloesParaLitros) // 391.9746
+
+// f)
+let xicaras = 450
+let litros2 = xicaras * (6 / 25)
+console.log("450 xc equivalem à: " + litros2) //108
+
+// g)
+let xicaras1 = prompt ("Quantas xícaras?")
+let litros3 = xicaras1 * (6 / 25)
+console.log(xicaras1 + " xc equivalem à: " + litros3) 
