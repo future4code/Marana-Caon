6d9b104ea526d8c3c01134d08d7c46efa2f6473a
Marana Caon <maranavaal@gmail.com>
2020-06-07 22:59:25 -0300

    exercício obrigatório

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -10,25 +10,27 @@
  * 
  * 
  */
-const cartas = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-const naipes = ["♦️", "♥️", "♣️", "♠️"]
-const usuario = 0
-const computador = 0
- // Sorteia uma carta
- const numero = cartas[Math.floor(Math.random() * 13)]
 
- // Sorteia um naipe
- const naipe = naipes[Math.floor(Math.random() * 4)]
-
+const carta1 = comprarCarta()
+const carta2 = comprarCarta()
+const carta3 = comprarCarta()
+const carta4 = comprarCarta()
+let pontuacaoUsuario = 0
+let pontuacaoComputador = 0
 
 console.log("Bem vindo ao jogo de Blackjack!")
 
 if(confirm("Quer iniciar uma nova rodada?")) {
-   const carta = comprarCarta(); 
-   console.log(carta.texto) 
-   console.log(carta.valor) 
-	console.log("Usuário - cartas: " , numero + naipe, "- pontuação")
-   console.log("Computador - cartas:" ,numero + naipe,"- pontuação")
+   pontuacaoUsuario = carta1.valor + carta2.valor
+   pontuacaoComputador = carta3.valor + carta4.valor
+	console.log(`Usuário - cartas: ${carta1.texto} ${carta2.texto} - pontuação: ${pontuacaoUsuario}`)
+   console.log(`Computador - cartas: ${carta3.texto} ${carta4.texto} - pontuação: ${pontuacaoComputador}`)
+} if (pontuacaoUsuario === pontuacaoComputador) {
+   console.log("Empate!")
+} else if (pontuacaoUsuario < pontuacaoComputador) {
+   console.log("O computador ganhou!")
+}  else if (pontuacaoUsuario > pontuacaoComputador) {
+   console.log("O usuário ganhou!")
 } else {
 	console.log("O jogo acabou!")
 }
