237fc7351d63b3cb8ec912a765bc2a6a614a0f4f
Marana Caon <maranavaal@gmail.com>
2020-06-10 17:03:45 -0300

    exercício principal blog - aula 15

---
+++ semana4/objetos/blog/index.js
@@ -0,0 +1,32 @@
+
+function rastreiaTecla(e) {
+    if (e.key === "Enter") {
+        meusInputs()
+    }
+}
+
+function meusInputs() {
+    let tituloDoPost = document.getElementById("titulo-post")
+    let autorDoPost = document.getElementById("autor-post")
+    let conteudoDoPost = document.getElementById("texto-post")
+
+    let publicacao = {
+        titulo: tituloDoPost.value,
+        autor: autorDoPost.value,
+        conteudo: conteudoDoPost.value
+    }
+
+
+let arrayDoPost = []
+
+arrayDoPost.push(publicacao)
+console.log(arrayDoPost)
+
+let postagem = document.getElementById("publicacao")
+postagem.innerHTML += ` ${publicacao.titulo} ${publicacao.autor} ${publicacao.conteudo}`
+
+tituloDoPost.value = ""
+autorDoPost.value = ""
+conteudoDoPost.value = ""
+
+}
