379766be260a7e9dec3f4b2cc94e8cdd636e35ac
Marana Caon <maranavaal@gmail.com>
2020-09-21 17:30:57 -0300

    exercícios aula 59

---
+++ semana19/aula-59/src/index.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+import dotenv from 'dotenv';
+import { AddressInfo } from "net";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// app.get("/teste", async (req: Request, res: Response) => {
+
+//     try {
+//         res.status(200).send("Oi, seu server está funcionando!");
+//     } catch (error) {
+
+//         res.status(400).send("ERRO");
+
+//     }
+// });
+
+const server = app.listen(3000, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost: ${address.port}`);
+    } else {
+        console.error(`Failure upon starting server.`);
+    }
+});
